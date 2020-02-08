5508692182b7b6c92aa60aded0bb601f40c3fc18
ehoversten <ehoversten@gmail.com>
2020-02-08 16:06:50 -0500

    updated schema tables to delete data related by foreign keys"

--- employee_tracker_schmea.sql
+++ employee_tracker_schmea.sql
@@ -7,23 +7,23 @@ USE employee_tracker;
 
 CREATE TABLE departments(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL(7, 2),
-    department_id INT,
-    FOREIGN KEY (department_id) REFERENCES departments(id)
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(7, 2) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employees(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (manager_id) REFERENCES employees(id)
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE CASCADE
 );
